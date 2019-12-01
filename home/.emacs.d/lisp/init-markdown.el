ad7c2d2ea9a0f6a75550d5b388776d7eaf3535ba
Charis-Nicolas Georgiou <cng_it@posteo.net>
2019-12-02 00:31:00 +0100

    added header and adapted doc configuration

--- home/.emacs.d/lisp/init-markdown.el
+++ home/.emacs.d/lisp/init-markdown.el
@@ -1,6 +1,42 @@
+;;; init-markdown ---  Markdown-mode configuration
+
+;; Copyright (C) 2019 Charis-Nicolas Georgiou
+
+;; Author: Charis-Nicolas Georgiou <cng_it@posteo.net>
+;; Created: 02 Dec 2019
+;; Version: 1.0
+;; Keywords: markdown
+;; X-URL: https://github.com/mokshasoul/init-markdown
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+;;
 (use-package markdown-mode
   :ensure t
-  :mode "\\.md\'")
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 (after-load 'whitespace-cleanup-mode
   (push 'markdown-mode whitespace-cleanup-mode-ignore-modes))
+
 (provide 'init-markdown)
+;;; init-markdown.el ends here
