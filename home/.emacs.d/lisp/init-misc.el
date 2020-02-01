ae381806dd6c4a41fbaf726f2597a1c35cb2e9fa
Charis-Nicolas Georgiou <cng_it@posteo.net>
2020-02-01 23:34:52 +0100

    added header and commentary

--- home/.emacs.d/lisp/init-misc.el
+++ home/.emacs.d/lisp/init-misc.el
@@ -1,5 +1,32 @@
-;;; Code:
+;;; init-misc --- MISCELLANEOUS PACKAGES
 
+;; Copyright (C) 2020 Charis-Nicolas Georgiou
+
+;; Author: Charis-Nicolas Georgiou <cng_it@posteo.net>
+;; Created: 01 Feb 2020
+;; Version: 1.0
+;; Keywords: crux misc
+;; X-URL: https://github.com/mokshasoul/dotfiles
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+;; Here are extra packages that provide QoL features
 ;;----------------------------------------------------------------------------
 ;; Misc config - yet to be placed in separate files
 ;;----------------------------------------------------------------------------
@@ -49,4 +76,5 @@
 ;;; CruX
 (use-package crux)
 (provide 'init-misc)
-;;; init-misc.el Ends here
+
+;;; init-misc.el ends here
