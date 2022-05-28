176a57b3c711b2071b2f32232d95f34e9518a47f
Charis-Nicolas Georgiou <cng_it@posteo.net>
2022-05-28 16:16:28 +0200

    Added some extra packages

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -1,14 +1,13 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(package-selected-packages
-     '(dap-mode git-modes flycheck-clj-kondo flycheck-pos-tip flycheck-clolyze fish-mode company-terraform pyenv tree-sitter-langs pyvenv-auto rg lsp-pascal envrc slime-company slime cask-mode flycheck-relint cl-libify package-lint-flymake highlight-quoted rainbow-mode macrostep aggressive-indent immortal-scratch auto-compile ipretty elisp-slime-nav flycheck-clojure cider elein cljsbuild-mode clojure-mode org-roam lsp-ivy company-lsp lsp lsp-pyright lsp-mode lsp-python-ms lsp-ui poetry use-package))
-  '(session-use-package t nil (session)))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-roam yasnippet-snippets yagist whitespace-cleanup-mode which-key wgrep web-mode vlf virtualenvwrapper use-package unfill undo-tree try tagedit systemd symbol-overlay ssh-config-mode smarty-mode session scss-mode scratch sass-mode regex-tool rainbow-delimiters poetry pipenv paredit-everywhere page-break-lines org-projectile org-cliplink org-bullets nov nlinum multiple-cursors mode-line-bell magit-gh-pulls lua-mode lsp-ui lsp-python-ms lsp-pyright lsp-ivy list-unicode-display leuven-theme js2-mode js-doc ivy-xref i3wm htmlize gnuplot git-timemachine ggtags fullframe flycheck-color-mode-line expand-region exec-path-from-shell evil emmet-mode emacsql-sqlite elpy editorconfig dynamic-spaces dsvn dotenv-mode dockerfile-mode docker-compose-mode docker diredfl diminish diff-hl csv-mode crux counsel-projectile company-php company-jedi company-auctex company-ansible command-log-mode calfw-org calfw browse-kill-ring beacon ansible-doc ansible ace-window)))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
