b7fa10ac6347e9590a0e287388e5fe93faf5c234
Charis-Nicolas Georgiou <charis.georgiou@soundcloud.com>
2022-05-30 14:25:38 +0200

    ripgrep

--- emacs/.emacs.d/lisp/init-grep.el
+++ emacs/.emacs.d/lisp/init-grep.el
@@ -1,13 +1,17 @@
 ;;; Code:
 (setq-default grep-highlight-matches t
-              grep-scroll-output t)
+  grep-scroll-output t)
+
 ;; If executable ag exists, download package
 (when (and (executable-find "ag")
-           (use-package ag :ensure t))
-	   (use-package wgrep-ag
-	     :ensure t
-	     :init
-	     (setq-default ag-highlight-search t)))
+        (use-package ag :ensure t))
+	(use-package wgrep-ag
+	  :ensure t
+	  :init
+	  (setq-default ag-highlight-search t)))
+
+(when (and (executable-find "rg")
+        (use-package rg :ensure t)))
 
 (provide 'init-grep)
-;;; init-grep Ends here
+;;; init-grep,el ends here
