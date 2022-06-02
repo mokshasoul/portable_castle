c1e5c83bf649c5f3aa4643e904294c5bdb91e0ea
Charis-Nicolas Georgiou <cng_it@posteo.net>
2022-06-02 08:10:16 +0200

    REFACTOR: css to init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -74,7 +74,19 @@
 
 ;;; WebDev Stuff
 (require 'init-web)
-(require 'init-css)
+;; Install SASS-Mode, assign to extensions, set indent on init
+(use-package sass-mode
+  :ensure t
+  :mode "\\.sass\\.erb"
+  :init
+  (setq css-indent-offset 2))
+;; Install SCSS-Mode assign file endings
+(use-package scss-mode
+  :ensure t
+  :mode "\\.scss"
+  :init
+  (setq-default scss-compile-at-save nil))
+
 (require 'init-js)
 ;;; Paredit
 (require 'init-paredit)
