9eb86ee168e79a58fb56e0bbd33e57fae72b9ec8
Charis-Nicolas Georgiou <charis.georgiou@soundcloud.com>
2022-06-02 09:42:52 +0200

    REFACTOR: move init-web to init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -63,8 +63,8 @@
 (require 'init-csv)
 ;; Languages
 (require 'init-python)
-;;; PHP
 
+;;; PHP
 (use-package php-mode)
 (use-package smarty-mode)
 
@@ -77,7 +77,40 @@
   :ensure t)
 
 ;;; WebDev Stuff
-(require 'init-web)
+(use-package web-mode
+  :ensure t
+  :mode (("\\.phtml\\'" . web-mode)
+          ("\\.tpl\\.php\\'" . web-mode)
+          ("\\.[agj]sp\\'" . web-mode)
+          ("\\.as[cp]x\\'" . web-mode)
+          ("\\.erb\\'" . web-mode)
+          ("\\.mustache\\'" . web-mode)
+          ("\\.html?\\'" . web-mode)
+          ("\\.djhtml?\\'" . web-mode))
+  :init
+  (setq web-mode-code-indent-offset 2
+    web-mode-markupt-indent-offset 2
+    web-mode-enable-auto-pairing nil))
+
+;; make web-mode play nice with smartparens taken from
+;; prelude
+;; (sp-with-modes '(web-mode)
+;;   (sp-local-pair "%" "%"
+;;                  :unless '(sp-in-string-p)
+;;                  :post-handlers '(((lambda (&rest _ignored)
+;;                                      (just-one-space)
+;;                                      (save-excursion (insert " ")))
+;;                                    "SPC" "=" "#")))
+;;   (sp-local-tag "%" "<% "  " %>")
+;;   (sp-local-tag "=" "<%= " " %>")
+;;   (sp-local-tag "#" "<%# " " %>")
+;;; EMMET is nice ;)
+(use-package emmet-mode
+  :requires web-mode
+  :hook web-mode
+  :init
+  (setq emmet-indentation 2))
+
 ;; Install SASS-Mode, assign to extensions, set indent on init
 (use-package sass-mode
   :ensure t
