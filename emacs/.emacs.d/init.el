c27f15c4cbda4bb1f1c51b38466daae23ab30291
Charis-Nicolas Georgiou <cng_it@posteo.net>
2022-06-04 15:05:44 +0200

    REFACTOR: js dev to init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -179,7 +179,32 @@
   :init
   (setq-default scss-compile-at-save nil))
 
-(require 'init-js)
+;; Javascript
+
+;;; Currently top javascript package for emacs
+(use-package js2-mode
+  :ensure t
+  :mode (("\\.js\\'" . js2-mode)
+         ("\\.pac\\'" . js2-mode))
+  :commands js2-mode
+  :interpreter "node"
+  :config
+  (setq mode-name "JS2")
+  (js2-imenu-extras-mode t)
+  (setq-default
+   js2-mode-indent-ignore-first-tab t))
+
+;; JSON support
+(use-package json-mode
+  :ensure t
+  :commands json-mode
+  :config
+  (bind-keys :map json-mode-map
+             ("C-c <tab>" . json-mode-beautify)))
+;; Adddinsx
+(use-package json-snatcher :ensure t)
+(use-package js-doc :ensure t)
+
 ;;; Paredit
 (require 'init-paredit)
 (require 'init-html)
