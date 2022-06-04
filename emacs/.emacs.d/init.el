3a1090c1c1f5391a48a7f61fbd43e2eece2a20a3
Charis-Nicolas Georgiou <cng_it@posteo.net>
2022-06-04 15:03:21 +0200

    REFACTOR: latex to init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -104,7 +104,28 @@
 (use-package php-mode)
 (use-package smarty-mode)
 
-(require 'init-latex)
+;;; LaTeX
+(use-package auctex
+  :mode ("\\.tex\\'" . TeX-latex-mode)
+  :hook ((LaTeX-mode . visual-line-mode)
+         (LaTeX-mode . LaTeX-math-mode)
+         (LaTeX-mode . auto-fill-mode)
+         (LaTeX-mode . turn-on-reftex)
+         (LaTeX-mode . flyspell-mode))
+  :custom
+  (custom-set-variables '(LaTeX-command "latex -synctex=1"))
+  :init
+  (add-hook 'TeX-after-compilation-finished-functions
+            #'TeX-revert-document-buffer)
+
+  (setq TeX-view-program-list
+        '(("PDF Viewer" "okular --unique %o#src:%n%b")))
+  (setq TeX-view-program-selection
+        '((output-pdf "PDF Viewer"))))
+
+(provide 'init-latex)
+;;; init-latex.el Ends here
+
 ;;; Devops Stuffnn
 (use-package ansible
   :ensure t)
