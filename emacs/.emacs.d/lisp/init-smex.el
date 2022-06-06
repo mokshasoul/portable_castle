fff8c33834c74da62169aecadefa6a9413830fea
Charis-Nicolas Georgiou <cng_it@posteo.net>
2022-06-06 17:44:44 +0200

    DELETE: smex

--- emacs/.emacs.d/lisp/init-smex.el
+++
@@ -1,6 +0,0 @@
-(use-package smex
-  :ensure t
-  :init
-  (setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
-  (global-set-key [remap execute-extended-command] 'smex))
-(provide 'init-smex)
