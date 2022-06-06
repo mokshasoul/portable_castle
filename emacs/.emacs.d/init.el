0a2ce8c3205bf7702b178f3acc818c926c991d36
Charis-Nicolas Georgiou <cng_it@posteo.net>
2022-06-06 17:38:04 +0200

    REFACTOR: gui settings to init

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -41,8 +41,40 @@
 (use-package monokai-theme)
 (add-to-list 'default-frame-alist '(font . "Hack-12"))
 (load-theme 'leuven t)
-(require 'init-theme)
-(require 'init-gui-frames)
+
+;;----------------------------------------------------------------------------
+;; Suppress GUI features
+;;----------------------------------------------------------------------------
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
+(setq inhibit-startup-screen t)
+;;----------------------------------------------------------------------------
+;; Window size and features
+;;----------------------------------------------------------------------------
+(when (fboundp 'menu-bar-mode)
+  (menu-bar-mode nil))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
+(when (fboundp 'pixel-scroll-mode)
+  (pixel-scroll-mode 1))
+(let ((no-border '(internal-border-width . 0)))
+  (add-to-list 'default-frame-alist no-border)
+  (add-to-list 'initial-frame-alist no-border))
+
+(setq frame-title-format
+      '((:eval (if (buffer-file-name)
+                   (abbreviate-file-name (buffer-file-name))
+                 "%b"))))
+;; Non-zero values for `line-spacing' can mess up ansi-term and co,
+;; so we zero it explicitly in those cases.
+(add-hook 'term-mode-hook
+          (lambda ()
+            (setq line-spacing 0)))
+
+(setq scroll-step 1)
+
 (require 'init-windows)
 ;; Better dired configuration, taken from purcell
 (require 'init-dired)
