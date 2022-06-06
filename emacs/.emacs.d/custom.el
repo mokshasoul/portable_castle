36c252bb8207f66c4644098f2060f1ff40ceb5ba
Charis-Nicolas Georgiou <cng_it@posteo.net>
2022-06-06 14:40:42 +0200

    new stuff from custom

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -1,14 +1,14 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(package-selected-packages
-     '(with-venv smartparens org-roam yasnippet-snippets yagist whitespace-cleanup-mode which-key wgrep web-mode vlf virtualenvwrapper use-package unfill undo-tree try tagedit systemd symbol-overlay ssh-config-mode smarty-mode session scss-mode scratch sass-mode regex-tool rainbow-delimiters poetry pipenv paredit-everywhere page-break-lines org-projectile org-cliplink org-bullets nov nlinum multiple-cursors mode-line-bell magit-gh-pulls lua-mode lsp-ui lsp-python-ms lsp-pyright lsp-ivy list-unicode-display leuven-theme js2-mode js-doc ivy-xref i3wm htmlize gnuplot git-timemachine ggtags fullframe flycheck-color-mode-line expand-region exec-path-from-shell evil emmet-mode emacsql-sqlite elpy editorconfig dynamic-spaces dsvn dotenv-mode dockerfile-mode docker-compose-mode docker diredfl diminish diff-hl csv-mode crux counsel-projectile company-php company-jedi company-auctex company-ansible command-log-mode calfw-org calfw browse-kill-ring beacon ansible-doc ansible ace-window))
-  '(session-use-package t nil (session)))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(smartparens rg yasnippet-snippets yagist whitespace-cleanup-mode which-key wgrep web-mode vlf virtualenvwrapper use-package unfill undo-tree try tagedit systemd symbol-overlay ssh-config-mode smarty-mode slime-company session scss-mode scratch sass-mode regex-tool rainbow-mode rainbow-delimiters poetry pipenv paredit-everywhere page-break-lines package-lint-flymake org-roam org-projectile org-cliplink org-bullets nov nlinum multiple-cursors mode-line-bell magit-gh-pulls lua-mode lsp-ui lsp-python-ms lsp-pyright lsp-ivy list-unicode-display leuven-theme js2-mode js-doc ivy-xref ipretty immortal-scratch i3wm htmlize highlight-quoted gnuplot git-timemachine git-modes ggtags fullframe flycheck-relint flycheck-color-mode-line flycheck-clojure fish-mode expand-region exec-path-from-shell evil emmet-mode elpy elisp-slime-nav elein editorconfig dynamic-spaces dsvn dotenv-mode dockerfile-mode docker-compose-mode docker diredfl diminish diff-hl csv-mode crux counsel-projectile company-terraform company-php company-jedi company-auctex company-ansible command-log-mode cljsbuild-mode cl-libify cask-mode calfw-org calfw browse-kill-ring beacon auto-compile ansible-doc ansible aggressive-indent ace-window))
+ '(session-use-package t nil (session)))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
