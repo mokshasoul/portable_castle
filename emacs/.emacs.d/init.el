a5d9476be9f578fb7b51de426d76d7f7e37a4ae0
Charis-Nicolas Georgiou <cng_it@posteo.net>
2022-06-06 22:18:14 +0200

    REFACTOR: slime setup to init

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -591,7 +591,43 @@ Call a second time to restore the original window configuration."
     (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1)))))
 ;;; LISP
 (require 'init-lisp)
-(require 'init-slime)
+(require-package 'slime)
+(push (expand-file-name "contrib" (file-name-directory (locate-library "slime"))) load-path)
+
+(use-package slime-company
+  :after (slime company)
+  :init
+  (setq  slime-company-completion 'fuzzy
+         slime-company-after-completion 'slime-company-just-one-space))
+
+
+
+;;; Lisp buffers
+
+(with-eval-after-load 'slime
+  (setq slime-protocol-version 'ignore)
+  (setq slime-net-coding-system 'utf-8-unix)
+  (let ((features '(slime-fancy slime-repl slime-fuzzy)))
+    (when (require 'slime-company nil t)
+      (push 'slime-company features))
+    (slime-setup features)) )
+
+
+;;; REPL
+
+(defun sanityinc/slime-repl-setup ()
+  "Mode setup function for slime REPL."
+  (sanityinc/lisp-setup))
+
+(with-eval-after-load 'slime-repl
+  ;; Stop SLIME's REPL from grabbing DEL, which is annoying when backspacing over a '('
+  (with-eval-after-load 'paredit
+    (define-key slime-repl-mode-map (read-kbd-macro paredit-backward-delete-key) nil))
+
+  ;; Bind TAB to `indent-for-tab-command', as in regular Slime buffers.
+  (define-key slime-repl-mode-map (kbd "TAB") 'indent-for-tab-command)
+
+  (add-hook 'slime-repl-mode-hook 'sanityinc/slime-repl-setup))
 ;;; CLOJURE
 (use-package clojure-mode)
 
